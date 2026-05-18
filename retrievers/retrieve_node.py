ba222e3fd3db592b22f614a78dbba1080d0b532e
Atharv Marathe <32080080+amarathe29@users.noreply.github.com>
2026-05-17 21:25:58 -0700

    Updates retrieve_node.py
    Removes unnecessary yaw function, adds feedback to retrieve_callback, adds state handling to retrieve_callback for all states except recovery, and saves start_pose to return to the start pose before returning to idle.

--- retrievers/retrieve_node.py
+++ retrievers/retrieve_node.py
@@ -98,11 +98,6 @@ class RetrieveNode(Node):
 
         self.ts.registerCallback(self.cam_callback)
 
-        # self.cam_info_sub = self.subscriber(
-        #     f"{self.get_namespace()}/camera/color/camera_info",
-        #     CameraInfo,
-        #     self.cam_info_callback,
-        # )
 
         # Set up publisher
         self.vel_pub = self.create_publisher(Twist, f"{self.get_namespace()}/cmd_vel", 10)
@@ -115,7 +110,7 @@ class RetrieveNode(Node):
             self.retrieve_callback,
         )
 
-        # Aruco tag recognition stuff
+        # AprilTag recognition stuff
         ARUCO_DICT = cv2.aruco.DICT_APRILTAG_25h9
         self.aruco_dict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT)
         self.aruco_params = cv2.aruco.DetectorParameters()
@@ -133,23 +128,23 @@ class RetrieveNode(Node):
         self.state = StateMachine.IDLE
         self.pose = None
         self.odom = None
+        self.start_pose_set = False
+        self._start_pose = None
         self.request_pose = None
         self.grab_pose = None
 
     def pose_callback(self, msg: Pose) -> None:
         self.logger.debug(f"Received Pose: {msg}")
         self.pose = msg
+        if not self.start_pose_set:
+            self._start_pose = self.pose
+            self.start_pose_set = True
 
     def odom_callback(self, msg: Odometry) -> None:
         self.logger.debug(f"Received Odometry: {msg}")
         self.odom = msg
-
-    # def cam_info_callback(self, msg: CameraInfo) -> None:
-    # if (self.camera_matrix is None) or (self.distortion_coeffs is None):
-    #     self.logger.info("Received camera info, saving camera matrix")
-    #     self.camera_matrix = np.array(msg.k, dtype=np.float64).reshape((3, 3))
-    #     self.distortion_coeffs = np.array(msg.d, dtype=np.float64)
-    #     self.logger.debug(f"Camera matrix: {self.camera_matrix}")
+        if self._start_pose is None:
+            self._start_pose = self.odom.pose.pose
 
     def cam_callback(
         self, color_msg: Image, depth_msg: Image, depth_info: CameraInfo
@@ -215,7 +210,7 @@ class RetrieveNode(Node):
                 // 4
             )
 
-            # find distance to block
+            # Find distance to block center
             depth_val = depth[marker_center_y, marker_center_x]
             if (
                 self.state == StateMachine.GRABBING
@@ -268,6 +263,9 @@ class RetrieveNode(Node):
     def retrieve_callback(self, goal_handle):
         self.logger.info(f"Received retrieve action goal: {goal_handle.request}")
 
+        feedback_msg = GoToBlock.Feedback()
+        feedback_msg.block_captured = False
+        
         if self.state == StateMachine.IDLE:
             self.request_pose = goal_handle.request.goal_pose
             self.state = StateMachine.NAVIGATING
@@ -289,10 +287,25 @@ class RetrieveNode(Node):
                 self.logger.info(
                     f"Reached grab pose, entering Grabbing state to attempt to grab block"
                 )
+        if self.state == StateMachine.GRABBING:
+            # TODO: This should be changed to be the output of a function from the camera
+            feedback_msg.block_captured = True
 
-    def yaw_from_quaternion(self, q: Quaternion) -> float:
-        _, _, yaw = euler_from_quaternion(q.x, q.y, q.z, q.w)
-        return np.float64(yaw)
+
+        # This state is just if we want the robot to return to the starting position
+        if self.state == StateMachine.RETURNING:
+            if self._start_pose is not None:
+                reached = self.go_to_pose(self._start_pose)
+                if reached:
+                    goal_handle.succeed()
+                    result = GoToBlock.Result()
+                    result.success = True
+                    result.end_pose = self.curr_pose
+                    self.state == StateMachine.IDLE
+                    return result
+
+        feedback_msg.curr_pose = self.curr_pose
+        goal_handle.publish_feedback(feedback_msg)
 
     def angle_wrap(self, angle: float) -> float:
         while angle > np.pi:
@@ -310,12 +323,13 @@ class RetrieveNode(Node):
         dy = target_pose.position.y - self.curr_pose.position.y
         distance = np.sqrt(dx**2 + dy**2)
         angle_to_target = np.arctan2(dy, dx)
-        current_yaw = self.yaw_from_quaternion(self.curr_pose.orientation)
+        q = self.curr_pose.orientation
+        _, _, current_yaw = euler_from_quaternion(q.x, q.y, q.z, q.w)
         angle_diff = self.angle_wrap(angle_to_target - current_yaw)
         desired_yaw_diff = self.angle_wrap(target_pose.orientation.z - current_yaw)
 
         cmd = Twist()
-        p = 0.5
+        p = 0.5 # Arbitrary gain cause why not
 
         if abs(angle_diff) > 0.1 and distance > 0.15:
             cmd.angular.z = p * angle_diff
@@ -340,7 +354,6 @@ class RetrieveNode(Node):
         else:
             self.logger.warning("No pose information available")
             return None
-
 
 def main():
     rclpy.init()
