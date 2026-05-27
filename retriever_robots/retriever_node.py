a2a34d160572b24c6e116b30684b7878b733a2ed
Atharv Marathe <atharv3232@gmail.com>
2026-05-27 15:47:28 -0700

    testing other controller

--- retriever_robots/retriever_node.py
+++ retriever_robots/retriever_node.py
@@ -222,7 +222,9 @@ class RetrieveNode(Node):
                     self.return_state = State.POSITIONING
                     self.state = State.RECOVERY
                     continue
-                reached = self.go_to_pose(self.grab_pose)
+                reached = self.go_to_pose(
+                    self.grab_pose, controller=self.pose_controller
+                )
                 if reached:
                     self.logger.info(
                         f"[{self.state.name}]Reached grab pose, entering GRABBING state to grab block"
