dc87e7954af96e61d00753e676cab01ba841ec69
zmeyer1 <zane.meyer@gmail.com>
2026-05-27 12:53:19 -0700

    shut up

--- retriever_robots/retriever_node.py
+++ retriever_robots/retriever_node.py
@@ -352,7 +352,7 @@ class RetrieveNode(Node):
         assert k > gamma, f"k = {k} must be greater than gamma = {gamma}"
         assert h > 0, f"h = {h} must be greater than 0"
 
-        self.logger.info(f"Using CLF controller to go to {target_pose}")
+        self.logger.info(f"Using CLF controller to go to {target_pose}", throttle_duration_sec=5.0)
         q_curr = self.curr_pose.orientation
         _, _, theta = euler_from_quaternion(q_curr.x, q_curr.y, q_curr.z, q_curr.w)
         q_desired = target_pose.orientation
