9380ec00f83acadbb848211e6fee1a1e64b4a055
Atharv Marathe <atharv3232@gmail.com>
2026-05-27 17:07:50 -0700

    I am a dum dum, controller please work

--- retriever_robots/retriever_node.py
+++ retriever_robots/retriever_node.py
@@ -422,6 +422,7 @@ class RetrieveNode(Node):
         # Angle to goal from goal frame
         theta_error_vec = np.arctan2(position_error[1], position_error[0])
         if e <= 0.025:
+            self.logger.info(f"Positional error is small, setting theta_erro_vec to 0")
             theta_error_vec = 0
 
         alpha = theta_error_vec - (theta - desired_theta)
