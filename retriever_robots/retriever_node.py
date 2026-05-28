5faa24b60a68108ceb17e3dc3acdfaf114d65626
zmeyer1 <zane.meyer@gmail.com>
2026-05-28 07:56:23 -0700

    tuning

--- retriever_robots/retriever_node.py
+++ retriever_robots/retriever_node.py
@@ -297,7 +297,7 @@ class RetrieveNode(Node):
                     cmd.angular.z = np.sign(self.recovery_pose.position.y) * max(
                         min(0.8 * abs(self.recovery_pose.position.y), 0.15), 0.05
                     )
-                elif self.recovery_pose.position.x > 0.45:
+                elif self.recovery_pose.position.x > 0.42:
                     self.logger.info(
                         f"Recovery pose is far away {self.recovery_pose.position.x}, moving towards it",
                         throttle_duration_sec=1.0,
