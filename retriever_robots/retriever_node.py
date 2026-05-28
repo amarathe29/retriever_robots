6753116af57c3cc5204c07adcbfea70ef85e6582
Atharv Marathe <atharv3232@gmail.com>
2026-05-27 17:18:48 -0700

    What is going on

--- retriever_robots/retriever_node.py
+++ retriever_robots/retriever_node.py
@@ -421,8 +421,6 @@ class RetrieveNode(Node):
 
         # Angle to goal from goal frame
         theta_error_vec = np.arctan2(position_error[1], position_error[0])
-        if e <= 0.025:
-            theta_error_vec = 0
 
         alpha = theta_error_vec - (theta - desired_theta)
 
