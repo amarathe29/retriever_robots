02909aab41c7549a8d8fb294182c0d057d7eea2e
zmeyer1 <zane.meyer@gmail.com>
2026-06-03 22:55:54 -0700

    iteration

--- retriever_robots/detect_block.py
+++ retriever_robots/detect_block.py
@@ -61,7 +61,7 @@ class DetectBlock(Node):
             ]
         )
         self.R_cam_angle_to_robot = create_rotation_matrix(pitch=30, units="degrees")
-        self.R_cam_to_robot = self.R_cam_angle_to_robot @ self.R_image_to_robot_axes
+        self.R_cam_to_robot = self.R_image_to_robot_axes @ self.R_cam_angle_to_robot
 
         self.static_broadcaster = tf2_ros.StaticTransformBroadcaster(self)
         self._broadcast_static_camera_transform()
