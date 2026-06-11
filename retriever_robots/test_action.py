e0a897d3efbae48e93ab1b57fafc4addafff6a67
zmeyer1 <zane.meyer@gmail.com>
2026-06-11 00:28:00 -0700

    he trusts me not

--- retriever_robots/test_action.py
+++ retriever_robots/test_action.py
@@ -72,7 +72,7 @@ class RetrieverActionTestNode(Node):
 def main():
     rclpy.init()
     point = [1.5, 3.0, 0.0]
-    stockpile = np.array([[0.9, 1.25]*4])
+    stockpile = np.array([[0.9, 1.25]]*4)
     node = RetrieverActionTestNode()
     quat = quaternion_from_euler(yaw=45, units="degrees")
     print(f"Going to point {point}")
